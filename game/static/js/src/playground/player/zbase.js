7341347ebd4a42e4c0d746f73c1d670b683f0106
Aozaki <2198182073@qq.com>
2023-11-04 18:45:45 +0800

    add listening_events and attack behavior

--- game/static/js/src/playground/player/zbase.js
+++ game/static/js/src/playground/player/zbase.js
@@ -7,16 +7,26 @@ class Player extends Balls_Game_Object{
         this.y = y;
         this.vx = 0;
         this.vy = 0;
+        this.damage_x = 0;
+        this.damage_y = 0;
+        this.damage_speed = 0;
         this.move_length = 0;
         this.radius = radius;
         this.color = color;
         this.speed = speed;
         this.is_me = is_me;
         this.eps = 0.01;
+        this.friction = 0.9;
+        this.spent_time = 0;
+        this.cur_skill = null;
     }
     start(){
         if(this.is_me){
             this.add_listening_events();
+        }else{
+            let tx = Math.random() * this.playground.width;
+            let ty = Math.random() * this.playground.height;
+            this.move_to(tx, ty);
         }
     }
     add_listening_events(){
@@ -27,8 +37,28 @@ class Player extends Balls_Game_Object{
         this.playground.game_map.$canvas.mousedown(function(e){
             if(e.which === 3){
                 outer.move_to(e.clientX, e.clientY);
+            }else if(e.which === 1){
+                if(outer.cur_skill === "fireball"){
+                    outer.shoot_fireball(e.clientX, e.clientY);
+                    outer.cur_skill = null;
+                }
             }
         });
+        $(window).keydown(function(e){
+            if(e.which === 81){ // q键
+                outer.cur_skill = "fireball";
+                return false;
+            }
+        });
+    }
+    shoot_fireball(tx, ty){
+        let x = this.x, y = this.y;
+        let radius = this.playground.height * 0.01;
+        let angle = Math.atan2(ty - this.y, tx - this.x);
+        let vx = Math.cos(angle), vy = Math.sin(angle);
+        let speed = this.playground.height * 0.5;
+        let move_length = this.playground.height * 1.5;
+        new FireBall(this.playground, this, x, y, radius, vx, vy, "orange", speed, move_length, this.playground.height * 0.01);
     }
     get_dist(x1, y1, x2, y2) {
         let dx = x2 - x1;
@@ -42,16 +72,58 @@ class Player extends Balls_Game_Object{
         this.vx = Math.cos(angle);
         this.vy = Math.sin(angle);
     }
+    is_attacked(angle, damage){
+        for (let i = 0; i < 10 + Math.random() * 5; i ++ ){
+            let x = this.x, y = this.y;
+            let radius = this.radius * Math.random() * 0.1;
+            let angle = Math.PI * 2 * Math.random();
+            let vx = Math.cos(angle), vy = Math.sin(angle);
+            let color = this.color;
+            let speed = this.speed * 10;
+            new Particle(this.playground, x, y, radius, vx, vy, color, speed);
+        }
+        this.radius -= damage;
+        if(this.radius < 10){
+            this.destroy();
+            return false;
+        }
+        this.damage_x = Math.cos(angle);
+        this.damage_y = Math.sin(angle);
+        this.damage_speed = damage * 100;
+        this.speed *= 1.2;
+
+
+    }
     update(){
-        if(this.move_length < this.eps){
-            this.move_length = 0;
+        this.spent_time += this.timedelta;
+        if(! this.is_me && this.spent_time > 4000 && Math.random() < 1 / 300.0){
+            let player = this.playground.players[Math.floor(Math.random() * this.playground.players.length)];
+
+            let tx = player.x + player.speed * this.vx * this.timedelta / 1000 * 0.3;
+            let ty = player.y + player.speed * this.vy * this.timedelta / 1000 * 0.3;
+            this.shoot_fireball(tx, ty);
+        }
+        if(this.damage_speed > this.eps){
             this.vx = this.vy = 0;
+            this.move_length = 0;
+            this.x += this.damage_x * this.damage_speed * this.timedelta / 1000;
+            this.y += this.damage_y * this.damage_speed * this.timedelta / 1000;
+            this.damage_speed *= this.friction;
         }else{
-            let moved = Math.min(this.move_length, this.speed * this.timedelta / 1000);
-            console.log(this.move_length, this.speed * this.timedelta / 1000);
-            this.x += this.vx * moved;
-            this.y += this.vy * moved;
-            this.move_length -= moved;
+            if(this.move_length < this.eps){
+                this.move_length = 0;
+                this.vx = this.vy = 0;
+                if(!this.is_me){
+                    let tx = Math.random() * this.playground.width;
+                    let ty = Math.random() * this.playground.height;
+                    this.move_to(tx,ty);
+                }
+            }else{
+                let moved = Math.min(this.move_length, this.speed * this.timedelta / 1000);
+                this.x += this.vx * moved;
+                this.y += this.vy * moved;
+                this.move_length -= moved;
+            }
         }
         this.render();
     }
--- game/static/js/src/playground/zbase.js
+++ game/static/js/src/playground/zbase.js
@@ -2,7 +2,7 @@ class Balls_Game_Playground {
     constructor(root){
         this.root = root;
         this.$playground = $(`
-<div class="balls-game-playground">游戏界面</div>
+<div class="balls-game-playground"></div>
 `);
         this.hide();
 
@@ -12,7 +12,15 @@ class Balls_Game_Playground {
         this.game_map = new GameMap(this);
         this.players = [];
         this.players.push(new Player(this,this.width / 2, this.height / 2, this.height * 0.05, "white", this.height * 0.15, true));
+
+        for (let i = 0; i < 5; i ++ ){
+            this.players.push(new Player(this,this.width / 2, this.height / 2, this.height * 0.05, this.get_random_color(), this.height * 0.15, false));
+        }
         this.start()
+    }
+    get_random_color(){
+        let colors = ["blue", "red", "green", "grey", "pink"];
+        return colors[Math.floor(Math.random() * 5)];
     }
     start(){
         this.add_listening_events();
